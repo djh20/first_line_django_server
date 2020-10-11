796a219f1f3373c15bebffa4f237e7469362ca51
Junho Do <djh20@kumoh.ac.kr>
2020-10-11 14:17:11 +0900

    init

--- post/views.py
+++ post/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Post
 
-def readAll(request):
-    datas = {}
-    if request.method == 'GET':
-        results = Post.objects.all()
-        post_num = 0
-        for result in results:
-            datas[post_num] = {'title' : result.title, 'content' : result.content}
-            post_num+= 1
-        print(datas)
+class MemberController :
+    def readAll(request):
+        datas = {}
+        if request.method == 'GET':
+            results = Post.objects.all()
+            post_num = 0
+            for result in results:
+                datas[post_num] = {'title' : result.title, 'content' : result.content}
+                post_num+= 1
+            print(datas)
+            return JsonResponse(datas)
         return JsonResponse(datas)
-    return JsonResponse(datas)
 
