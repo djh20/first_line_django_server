f12ac45a555504b3b66c5c0d9a1919abfe1730db
Blackstarez <qssz1326@naver.com>
2020-11-16 20:47:32 +0900

    댓글등록 - 알림등록하도록 추가 notice - 알림등록 함수 추가

--- notice/models.py
+++ notice/models.py
@@ -5,6 +5,7 @@ class Notice(models.Model):
     receiver_id = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='receiver_id')
     sender_id = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='sender_id')
     text = models.TextField(max_length=3000)
+    source_url = models.TextField(max_length=100, null=True, blank=True)
     send_datetime = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default = False)
     
@@ -15,5 +16,14 @@ class Notice(models.Model):
             'sender_id' : self.sender_id.id,
             'send_datetime' : self.send_datetime.strftime("%y.%m.%d %p %I:%M"),
             'text' : self.text,
-            'is_read' : str(self.is_read)
+            'is_read' : str(self.is_read),
+            'source_url' : self.source_url
+        }
+
+    def get_for_user(self):
+        return {
+            'notice_id' : self.notice_id,
+            'send_datetime' : self.send_datetime.strftime("%y.%m.%d %p %I:%M"),
+            'text' : self.text,
+            'source_url' : self.source_url
         }
