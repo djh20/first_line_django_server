6d1047572263791135ddc24d451a30851ed6af10
Blackstarez <qssz1326@naver.com>
2020-11-22 17:52:58 +0900

    keyword변경 후 migrate적용

--- keyword_/models.py
+++ keyword_/models.py
@@ -6,9 +6,9 @@ class Keyword(models.Model):
     keyword = models.CharField(max_length = 10, primary_key=True)
     registrator = models.ForeignKey(Member, on_delete=models.CASCADE)
     registration_date = models.DateTimeField(auto_now_add=True)
-    recent_used_date = models.DateField()                       # 최근 사용일
-    suggest_date = models.DateField()                           # 사용 예정일
-    suggest_amount = models.IntegerField(default=0)             # 키워드 사용 횟수  - 키워드를 사용해서 작성한 글의 갯수
+    recent_used_date = models.DateField(null = True)                       # 최근 사용일
+    suggest_date = models.DateField(default=datetime.date.today)           # 사용 예정일
+    suggest_amount = models.IntegerField(default=0)                        # 키워드 사용 횟수  - 키워드를 사용해서 작성한 글의 갯수
 
     def get_keyword(self):
         return self.keyword
